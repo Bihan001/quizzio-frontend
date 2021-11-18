b2e5c530e093956f202f5b702b5775e0a8fd94dd
Bihan001 <bhchakraborty001@gmail.com>
2021-11-18 23:26:49 +0530

    added components

---
+++ src/components/multi-select-dropdown/index.jsx
@@ -0,0 +1,60 @@
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+const getStyles = (opt, value, theme) => {
+  return {
+    fontWeight: value.indexOf(opt) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium,
+  };
+};
+
+const MultiSelect = (props) => {
+  const { value, onChange, options, label, required, style, disabled, labelStyle, labelClassName } = props;
+  const theme = useTheme();
+
+  return (
+    <>
+      <Typography
+        align='left'
+        variant='p'
+        style={{ color: disabled ? '#D9D7D7' : null, ...labelStyle }}
+        className={labelClassName}
+      >
+        {label}
+        {required ? <span style={{ color: 'red' }}>*</span> : null}
+      </Typography>
+      <br />
+      <Select
+        multiple
+        displayEmpty
+        required={required}
+        value={value}
+        size='small'
+        onChange={onChange}
+        input={<OutlinedInput />}
+        style={{ marginTop: '0.5rem', width: '300px', ...style }}
+        renderValue={(selected) => (
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+            {selected.length === 0 && 'Placeholder'}
+            {selected.length > 0 && selected.map((value) => <Chip key={value} label={value} />)}
+          </Box>
+        )}
+      >
+        {options.map((opt) => (
+          <MenuItem key={opt} value={opt} style={getStyles(opt, value, theme)}>
+            {opt}
+          </MenuItem>
+        ))}
+      </Select>
+    </>
+  );
+};
+
+export default MultiSelect;
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,24 +1,28 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import BookIcon from '@mui/icons-material/Book';
+
 const Navbar = (props) => {
   const { toggleTheme } = props;
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='sticky'>
-        <Toolbar variant='dense'>
-          <BookIcon sx={{ mr: 2 }} />
-          <Typography variant='h6' color='inherit' component='div' sx={{ flexGrow: 1 }}>
-            Exam Simulation
-          </Typography>
-          <Button style={{ color: 'white' }} onClick={() => toggleTheme()}>
-            Toggle dark mode
-          </Button>
-        </Toolbar>
+      <AppBar position='sticky' style={{ padding: '0.5rem 0' }}>
+        <Container maxWidth='xl'>
+          <Toolbar variant='dense'>
+            <BookIcon sx={{ mr: 2 }} />
+            <Typography variant='h6' color='inherit' component='div' sx={{ flexGrow: 1 }}>
+              Exam Simulation
+            </Typography>
+            <Button style={{ color: 'white' }} onClick={() => toggleTheme()}>
+              Toggle dark mode
+            </Button>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
