157f8769800424c37c64476ad0fb8e28bfa2ba7e
Bihan001 <bhchakraborty001@gmail.com>
2021-11-19 13:06:52 +0530

    added api setup

--- src/api/user.js
+++ src/api/user.js
@@ -4,8 +4,10 @@ import { axios, headers } from './config';
  * @param {string} email User's Email
  * @param {string} password User's Password
  * @returns Promise<AxiosResponse<any, any>>
- * @example signin('bihan', 'abcdefgh').then(res => console.log(res.data)).catch(err => console.log(err))
+ * @example
+ * import {signin} from 'api/user';
+ * signin('bihan', 'abcdefgh').then(res => console.log(res.data)).catch(err => console.log(err));
  */
 export const signin = (email, password) => {
-  return axios.post('https://www.google.com', { email, password }, headers);
+  return axios.post('/signin', { email, password }, headers);
 };
