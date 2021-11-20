29db9c70fbba4c44b829ffa5b56817ac74f4692c
Ankur Saha <ankursahamoto@gmail.com>
2021-11-20 20:43:16 +0530

    home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,6 +10,26 @@ import { Grid, Typography, Paper, Input, Box, Button } from '@mui/material';
 import useStyles from './styles';
 import { getAllUpcomingExams } from 'api/exam';
 import Carousel from 'components/Carousel';
+import ExamDetailsCard from 'components/ExamDetailsCard/ExamDetailsCard';
+import TextInputField from 'components/text-input-field';
+import DatePicker from 'components/date-time-picker/date';
+import TimePicker from 'components/date-time-picker/time';
+import MultiSelect from 'components/multi-select-dropdown';
+import DropdownField from 'components/dropdown-field';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import {
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Dialog,
+} from 'components/dialog';
+
+const examVisibilities = [
+  { label: 'Public', value: 'public' },
+  { label: 'Private', value: 'private' },
+];
+
+const examStatuses = ['Scheduled', 'Ongoing', 'Finished'];
 
 const Home = () => {
   const [upcomingExams, setUpcomingExams] = useState([]);
@@ -27,11 +47,62 @@ const Home = () => {
   }, []);
 
   return (
-    <>
+    <div className={classes.homePage}>
       <section className={classes.carouselSection}>
         <Carousel data={upcomingExams} type="exam-banner" />
       </section>
-    </>
+      <section className={classes.examListSection}>
+        <div className={classes.sectionHeading}>Events and Contests</div>
+        <ExamsListWithFilter />
+      </section>
+      <section className={classes.examListSection}>
+        <div className={classes.sectionHeading}>More Exams</div>
+        <ExamsListWithFilter />
+      </section>
+    </div>
   );
 };
 export default Home;
+
+const ExamsListWithFilter = () => {
+  const [showFilters, setShowFilters] = useState(false);
+  const [selectedFilters, setSelectedFilters] = useState({
+    startDate: null,
+    examVisibility: null,
+    examStatus: null,
+  });
+
+  const handleFilterChange = (e) =>
+    setSelectedFilters((f) => ({ ...f, [e.target.name]: e.target.value }));
+
+  const hideFilters = () => setShowFilters(false);
+  return (
+    <>
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          border: '0px solid red',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          style={{
+            marginTop: '1rem',
+          }}
+        >
+          {new Array(3).fill(0).map((exam, i) => (
+            <ExamDetailsCard
+              style={{
+                marginBottom: i === 4 ? '0.3rem' : '2rem',
+                width: '80rem',
+                borderRadius: 0,
+              }}
+              data={{ name: 'asd', email: 'ankur' }}
+            />
+          ))}
+        </Box>
+      </Box>
+    </>
+  );
+};
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,13 +1,35 @@
 import { makeStyles } from '@mui/styles';
 
 export default makeStyles((theme) => ({
+  homePage: {
+    backgroundColor:
+      theme.palette.mode === 'dark'
+        ? theme.palette.common.black
+        : theme.palette.grey[200],
+  },
   heading: {
     fontSize: '3.2rem',
     textAlign: 'center',
     marginBottom: '2rem',
   },
+  sectionHeading: {
+    fontSize: '3rem',
+    width: '80rem',
+    border: '0px solid red',
+    margin: '1.5rem auto',
+    fontWeight: '600',
+    color: theme.palette.grey[600],
+  },
   carouselSection: {
     padding: '4rem',
+    backgroundColor:
+      theme.palette.mode === 'dark'
+        ? theme.palette.grey[900]
+        : theme.palette.grey[100],
+    marginBottom: '3rem',
+  },
+  examListSection: {
+    paddingTop: '4rem',
     backgroundColor:
       theme.palette.mode === 'dark'
         ? theme.palette.grey[900]
